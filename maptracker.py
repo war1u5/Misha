b4ddf9ae35eb54a0bb6c8d08c6d2b358492db24d
war1u5 <marius.wsf@gmail.com>
2023-10-31 16:57:34 +0200

    removed comments

--- maptracker.py
+++ maptracker.py
@@ -17,23 +17,19 @@ class MapWidget(QWidget):
         self.setGeometry(300, 300, 800, 600)
         self.setWindowTitle('Map')
 
-        # Create folium map
         m = folium.Map(location=[positions[0][0], positions[0][1]], zoom_start=15)
 
-        # Add a marker for each position
         for position in positions:
             folium.Marker([position[0], position[1]]).add_to(m)
 
-        # Draw a line connecting the points
+
         folium.PolyLine(positions, color="red", weight=2.5, opacity=1).add_to(m)
 
-        # Save it to tmp file and re-open file as base64
         data = io.BytesIO()
         m.save(data, close_file=False)
         data.seek(0)
         html = data.read()
 
-        # Set Map to QWebEngineView
         self.webEngineView = QWebEngineView(self)
         self.webEngineView.setHtml(html.decode())
         self.webEngineView.setGeometry(50, 50, 700, 500)
@@ -43,7 +39,6 @@ class MapWidget(QWidget):
 def main():
     app = QApplication(sys.argv)
 
-    # Set your GPS coordinates here
     positions = [[44.4268, 26.1025], [44.4278, 26.1035], [44.4288, 26.1045]]
 
     ex = MapWidget(positions)

a3d276fda48b0fc3b07d3997c51d5247f035ab7a
war1u5 <marius.wsf@gmail.com>
2023-11-09 22:18:59 +0200

    json transfer over kafka channel

--- backend/kafka_consumer.py
+++ backend/kafka_consumer.py
@@ -6,7 +6,7 @@ class KafkaConsumerWrapper:
     def __init__(self, kafka_servers, kafka_topic):
         self.consumer = KafkaConsumer(kafka_topic,
                                       bootstrap_servers=kafka_servers,
-                                      value_deserializer=lambda v: json.loads(v.decode('utf-8')))
+                                      value_deserializer=lambda v: v.decode('utf-8'))
 
     def start_consuming(self):
         for message in self.consumer:

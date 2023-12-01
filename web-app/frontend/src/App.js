528dbbe7a6f358f397d4e84825bf42b44f46fecf
war1u5 <marius.wsf@gmail.com>
2023-12-01 21:20:04 +0200

    working on the web app

--- web-app/frontend/src/App.js
+++ web-app/frontend/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Home from './containers/Home';
+import Login from './containers/Login';
+import Signup from './containers/Signup';
+import Activate from './containers/Activate';
+import ResetPassword from './containers/ResetPassword';
+import ResetPasswordConfirm from './containers/ResetPasswordConfirm';
+
+import Layout from './hocs/Layout';
+
+const App = () => (
+    <Router>
+        <Layout>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/signup' element={<Signup />} />
+                <Route path='/activate/:uid/:token' element={<Activate />} />
+                <Route path='/reset-password' element={<ResetPassword />} />
+                <Route path='/password/reset/confirm/:uid/:token' element={<ResetPasswordConfirm />} />
+            </Routes>
+        </Layout>
+    </Router>
+);
 
 export default App;

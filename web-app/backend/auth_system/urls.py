764a52b8cfd8fb789584c43ebd3a22c96981e2c0
war1u5 <marius.wsf@gmail.com>
2023-12-01 16:36:46 +0200

    implemented backend

--- web-app/backend/auth_system/urls.py
+++ web-app/backend/auth_system/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for web_misha project.
+URL configuration for auth_system project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -14,9 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
+
+urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
--- web-app/backend/auth_system/wsgi.py
+++ web-app/backend/auth_system/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for web_misha project.
+WSGI config for auth_system project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_misha.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auth_system.settings')
 
 application = get_wsgi_application()

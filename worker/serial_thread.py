d9c2f247468977d230b9a736554bd3e7376df659
war1u5 <marius.wsf@gmail.com>
2023-12-11 17:47:16 +0200

    prepared the second topic

--- worker/serial_thread.py
+++ worker/serial_thread.py
@@ -4,7 +4,7 @@ import serial.serialutil
 import json
 
 from Misha.worker.kafka_producer import KafkaProducerWrapper
-from worker_config import WORKER_ID, BAUD_RATE, GPS_DATA_KAFKA_TOPIC, KAFKA_SERVERS
+from worker_config import WORKER_ID, BAUD_RATE, GPS_DATA_KAFKA_TOPIC, ALL_DATA_KAFKA_TOPIC, KAFKA_SERVERS
 
 
 class SerialThread(QThread):
@@ -13,24 +13,35 @@ class SerialThread(QThread):
     def __init__(self, com_port):
         QThread.__init__(self)
         self.com_port = com_port
-        self.kafka_producer = KafkaProducerWrapper(KAFKA_SERVERS, GPS_DATA_KAFKA_TOPIC)
+        self.kafka_producer_gps = KafkaProducerWrapper(KAFKA_SERVERS, GPS_DATA_KAFKA_TOPIC)
+        self.kafka_producer_all = KafkaProducerWrapper(KAFKA_SERVERS, ALL_DATA_KAFKA_TOPIC)
 
     def run(self):
         global ser
         try:
-            ser = serial.Serial(self.com_port, BAUD_RATE, timeout=1)
-            while True:
-                try:
-                    data = ser.readline().decode().strip()
-                except UnicodeDecodeError:
-                    continue
+            with serial.Serial(self.com_port, BAUD_RATE, timeout=1) as ser:
+                while True:
+                    try:
+                        data = ser.readline().decode().strip()
+                    except UnicodeDecodeError:
+                        continue
 
-                if data:  # if data is not empty
-                    message = {'worker_id': WORKER_ID, 'data': data}
-                    self.signal.emit(message)
-                    json_object = self.transform_message(message)
-                    # print(json_object)
-                    self.kafka_producer.send_message(bytes(f'{json_object}', 'UTF-8'))
+                    if data:
+                        message = {'worker_id': WORKER_ID, 'data': data}
+                        self.signal.emit(message)
+
+                        json_object = self.transform_message(message)
+                        json_gps = self.extract_gps_data(message)
+
+                        try:
+                            self.kafka_producer_gps.send_message(bytes(f'{json_gps}', 'UTF-8'))
+                        except Exception as kafka_error:
+                            print(f"Error sending message to GPS_DATA_KAFKA_TOPIC: {kafka_error}")
+
+                        try:
+                            self.kafka_producer_all.send_message(bytes(f'{json_object}', 'UTF-8'))
+                        except Exception as kafka_error:
+                            print(f"Error sending message to ALL_DATA_KAFKA_TOPIC: {kafka_error}")
 
         except serial.serialutil.SerialException as e:
             self.signal.emit(f"Error: {e}")
@@ -55,19 +66,14 @@ class SerialThread(QThread):
         }
         return json.dumps(json_message)
 
-    # def extract_gps_data(self, message):
-    #     data = message['data']
-    #     split_message = data.split("'")[1].split(", ")
-    #     json_message = {
-    #         'worker_id': message['worker_id'],
-    #         'hello': int(split_message[0].split(": ")[1]),
-    #         'Valid': int(split_message[1].split(": ")[1]),
-    #         'Lat': float(split_message[2].split(": ")[1]),
-    #         'Lng': float(split_message[3].split(": ")[1]),
-    #         'Satellites': int(split_message[4].split(": ")[1]),
-    #         'Timestamp': int(split_message[5].split(": ")[1]),
-    #         'Date': split_message[6].split(": ")[1],
-    #         'Time': split_message[7].split(": ")[1],
-    #         'RSSI': int(data.split()[-1])
-    #     }
-    #     return json.dumps(json_message)
+    def extract_gps_data(self, message):
+        data = message['data']
+        split_message = data.split("'")[1].split(", ")
+        json_message = {
+            'worker_id': message['worker_id'],
+            'hello': int(split_message[0].split(": ")[1]),
+            'Valid': int(split_message[1].split(": ")[1]),
+            'Lat': float(split_message[2].split(": ")[1]),
+            'Lng': float(split_message[3].split(": ")[1]),
+        }
+        return json.dumps(json_message)
